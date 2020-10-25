d52b6f502458458400541f40f415dd347ed0c561
Alvaro Quintana <alvaroqp@gmail.com>
2020-10-25 17:45:46 +0100

    share result

--- app/src/main/java/com/alvaroquintana/adivinaperro/ui/result/ResultActivity.kt
+++ app/src/main/java/com/alvaroquintana/adivinaperro/ui/result/ResultActivity.kt
@@ -2,10 +2,13 @@ package com.alvaroquintana.adivinaperro.ui.result
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import com.alvaroquintana.adivinaperro.BuildConfig
 import com.alvaroquintana.adivinaperro.R
 import com.alvaroquintana.adivinaperro.base.BaseActivity
 import com.alvaroquintana.adivinaperro.common.startActivity
 import com.alvaroquintana.adivinaperro.ui.select.SelectActivity
+import com.alvaroquintana.adivinaperro.utils.log
 import com.alvaroquintana.adivinaperro.utils.setSafeOnClickListener
 import kotlinx.android.synthetic.main.app_bar_layout.*
 
@@ -26,5 +29,26 @@ class ResultActivity : BaseActivity() {
             }
         }
         toolbarTitle.text = getString(R.string.resultado_screen_title)
+        imageShare.visibility = View.VISIBLE
+        imageShare.setSafeOnClickListener {
+            shareApp()
+        }
+    }
+
+    private fun shareApp() {
+        try {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.app_name))
+            var shareMessage = getString(R.string.share_message)
+            shareMessage =
+                """
+                ${shareMessage}https://play.google.com/store/apps/details?id=${BuildConfig.APPLICATION_ID}
+                """.trimIndent()
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage)
+            startActivity(Intent.createChooser(shareIntent, getString(R.string.choose_one)))
+        } catch (e: Exception) {
+            log(getString(R.string.share), e.toString())
+        }
     }
 }
