538b5fcb3b0280b32ebb93f1eef4240f869e1527
Alvaro Quintana <alvaroqp@gmail.com>
2026-04-25 19:28:33 +0200

    feat: scaffold KMP multiplatform architecture (#5)
    Migrate project from Android-only to Kotlin Multiplatform:
    - Create :core KMP module with domain entities (Dog, GameMode, App)
    - Convert :data and :usecases from java-library to KMP modules
    - Move sources to commonMain/kotlin, tests to androidHostTest/kotlin
    - Use AGP 9 KMP plugin (com.android.kotlin.multiplatform.library)
    - Convert all build files from Groovy to Kotlin DSL (.kts)
    - Add iOS targets (iosArm64, iosSimulatorArm64) to all shared modules
    - Remove old :domain module (replaced by :core)
    - Update dependencyResolutionManagement in settings.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,13 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.android.kmp.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.firebase.crashlytics) apply false
     alias(libs.plugins.kotlin.serialization) apply false
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url "https://jitpack.io" }
-    }
-}
-
-tasks.register('clean', Delete) {
-    delete rootProject.buildDir
 }
---
+++ data/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.kmp.library)
+}
+
+kotlin {
+    android {
+        namespace = "com.alvaroquintana.data"
+        compileSdk = 36
+        minSdk = 23
+
+        withHostTestBuilder {}
+    }
+
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(project(":core"))
+            implementation(libs.kotlinx.coroutines.core)
+        }
+        getByName("androidHostTest").dependencies {
+            implementation(libs.junit)
+            implementation(libs.mockk)
+            implementation(libs.kotlinx.coroutines.test)
+        }
+    }
+}
